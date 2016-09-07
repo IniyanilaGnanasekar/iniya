6c8d022b286059d6c38a8b41183bd73af73650f2
IniyanilaGnanasekar <eniyashree20@gmail.com>
2016-09-07 12:09:47 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ double n;
+printf("enter the number');
+scanf("%d",&n);
+if(n==0.0)
+{
+printf('the number is 0');
+elseif(n<0.0)
+{
+printf("the number is negative
+}
+else
+printf("the number is positive");
+}
+}
